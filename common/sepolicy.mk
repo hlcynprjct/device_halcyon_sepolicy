b942a2dcbaf20fc78ee0b19f8bc135e696547a0e
Michael Bestas <mkbestas@gmail.com>
2021-11-10 23:32:44 +0200

    sepolicy: legacy: Remove legacy sepolicy leftover
    Change-Id: I2913565cc402b29acd380015b049ccf8d4e9959d
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -32,9 +32,6 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
     device/halcyon/sepolicy/common/vendor
 endif
 
-# Selectively include legacy rules defined by the products
--include device/halcyon/sepolicy/legacy-common/sepolicy.mk
-
 # Include atv rules on atv product
 ifeq ($(PRODUCT_IS_ATV), true)
 include device/halcyon/sepolicy/atv/sepolicy.mk
