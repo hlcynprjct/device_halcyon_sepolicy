289dab6ba0f873aef17c66bd739af80a8f478aa7
HeroBuxx <me@buxxed.tk>
2023-02-04 07:57:34 +0000

    SEPolicy: Initial changes for Halcyon
    Signed-off-by: HeroBuxx <me@buxxed.tk>

--- atv/sepolicy.mk
+++ atv/sepolicy.mk
@@ -1,3 +1,4 @@
+y
 #
 # This policy configuration will be used by atv products that
 # inherit from Lineage
@@ -5,8 +6,8 @@
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/atv/vendor
+    device/halcyon/sepolicy/atv/vendor
 endif
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/atv/private
+   device/halcyon/sepolicy/atv/private
--- exynos/sepolicy.mk
+++ exynos/sepolicy.mk
@@ -10,14 +10,14 @@ endif
 endif
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/exynos/private
+    device/halcyon/sepolicy/exynos/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/exynos/dynamic \
-    device/lineage/sepolicy/exynos/system
+    device/halcyon/sepolicy/exynos/dynamic \
+    device/halcyon/sepolicy/exynos/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/exynos/dynamic \
-    device/lineage/sepolicy/exynos/vendor
+    device/halcyon/sepolicy/exynos/dynamic \
+    device/halcyon/sepolicy/exynos/vendor
 endif
